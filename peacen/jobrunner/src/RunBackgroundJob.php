03f1180960a778421bf79d76e9a9a7890638c9f6
Peace Ngara <1544123+Peace-N@users.noreply.github.com>
2024-11-18 00:46:55 +0200

    Update RunBackgroundJob.php

--- peacen/jobrunner/src/RunBackgroundJob.php
+++ peacen/jobrunner/src/RunBackgroundJob.php
@@ -46,29 +46,30 @@ class RunBackgroundJob
 
         try {
 
-            $instance = new $this->class(); ////////named domain or namespace e.g \m\c\myclass.php
-            $instance->{$this->method}(...$this->params);
-            //call_user_func_array(array($instance, "MethodName"), $myArgs); // can be used as well
-
-            $log = "JUB RUNNER STARTED: CLASS:" . $this->class . ' -  METHOD: ' . $this->method . ' - ' . date("F j, Y, g:i a") . PHP_EOL .
-                "Attempt: " . ($this->class . ' | ' . $this->method) . PHP_EOL .
-                "User: " . 'Authenticated User??' . PHP_EOL .
-                "-------------------------" . PHP_EOL;
-            file_put_contents(__DIR__ . '/logs/background_job_info_log.log', $log, FILE_APPEND);
+            $pid = pcntl_fork();
+            if ($pid == -1) {
+                $this->genericMessage('FAILED STARTING RUNNER - COULD NOT START SEPARATE THREAD');
+                exit("Error forking...\n");
+            }
+            else if ($pid == 0) {
+                $this->execute_task($pid);
+                exit();
+            }
 
         } catch (\Exception $exception) {
-            //retry attempts --- delays --- priorities
-
-            $log = "JUB RUNNER FAILED: CLASS:" . $this->class . ' -  METHOD: ' . $this->method . ' - ' . date("F j, Y, g:i a") . PHP_EOL .
-                "Attempt: " . ($this->class . ' | ' . $this->method) . '|' . PHP_EOL .
-                "Reason: " . $exception->getMessage() . PHP_EOL .
-                "-------------------------" . PHP_EOL;
-            file_put_contents(__DIR__ . '/logs/background_jobs_errors.log', $log, FILE_APPEND);
+            $this->genericMessage('FAILED STARTING RUNNER ' . $exception->getMessage());
         }
 
         ///check completion
 
 
+    }
+
+    public function execute_task($task_id) {
+        $this->genericMessage('STARTING JOB RUNNER ON - ');
+        $instance = new $this->class();
+        $instance->{$this->method}(...$this->params);
+        $this->genericMessage('COMPLETED JOB RUNNER ON - ');
     }
 
     /**
@@ -84,17 +85,9 @@ class RunBackgroundJob
             }
 
         } catch (\Exception $e) {
-            $log = "JUB RUNNER FAILED: CLASS:" . $this->class . ' -  METHOD: ' . $this->method . ' - ' . date("F j, Y, g:i a") . PHP_EOL .
-                "Attempt: " . ($this->class . ' | ' . $this->method) . '|' . PHP_EOL .
-                "Reason: " . 'Invalid File Path | File not readable' . PHP_EOL .
-                "-------------------------" . PHP_EOL;
-            file_put_contents(__DIR__ . '/logs/background_jobs_errors.log', $log, FILE_APPEND);
+            $this->genericMessage('SUPPLIED PATH UNREADABLE - ');
         }
     }
-
-    /**
-     * @return false|void
-     */
 
     /**
      * @return false|void
@@ -102,24 +95,15 @@ class RunBackgroundJob
     public function validateClass()
     {
 
-
         /// Validate class Name
         if (!preg_match('/^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*$/', $this->class)) {
-            $log = "JUB RUNNER FAILED: CLASS:" . $this->class . ' -  METHOD: ' . $this->method . ' - ' . date("F j, Y, g:i a") . PHP_EOL .
-                "Attempt: " . ($this->class . ' | ' . $this->method) . '|' . PHP_EOL .
-                "Reason: " . 'Class Name Resolution Problems - Check Class Name' . PHP_EOL .
-                "-------------------------" . PHP_EOL;
-            file_put_contents(__DIR__ . '/logs/background_jobs_errors.log', $log, FILE_APPEND);
+            $this->genericMessage('ERROR CHECK CLASS NAME - ');
             return false;
         }
 
         // check class existence
         if (!class_exists($this->class)) {
-            $log = "JUB RUNNER FAILED: CLASS:" . $this->class . ' -  METHOD: ' . $this->method . ' - ' . date("F j, Y, g:i a") . PHP_EOL .
-                "Attempt: " . ($this->class . ' | ' . $this->method) . '|' . PHP_EOL .
-                "Reason: " . 'Class Name does not exist - Check Class Name' . PHP_EOL .
-                "-------------------------" . PHP_EOL;
-            file_put_contents(__DIR__ . '/logs/background_jobs_errors.log', $log, FILE_APPEND);
+            $this->genericMessage('ERROR CHECK CLASS EXISTS - ');
         }
 
 
@@ -131,25 +115,28 @@ class RunBackgroundJob
     public function validateMethod()
     {
         if (!preg_match('/^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*$/', $this->method)) {
-            $log = "JUB RUNNER FAILED: CLASS:" . $this->class . ' -  METHOD: ' . $this->method . ' - ' . date("F j, Y, g:i a") . PHP_EOL .
-                "Attempt: " . ($this->class . ' | ' . $this->method) . '|' . PHP_EOL .
-                "Reason: " . 'Method Name Resolution Problems - Check Method Name' . PHP_EOL .
-                "-------------------------" . PHP_EOL;
-            file_put_contents(__DIR__ . '/logs/background_jobs_errors.log.log', $log, FILE_APPEND);
+            $this->genericMessage('ERROR CHECK METHOD NAME - ');
             return false;
         }
 
         // check method existence
         if (!function_exists($this->method)) {
-            $log = "JUB RUNNER FAILED: CLASS:" . $this->class . ' -  METHOD: ' . $this->method . ' - ' . date("F j, Y, g:i a") . PHP_EOL .
-                "Attempt: " . ($this->class . ' | ' . $this->method) . '|' . PHP_EOL .
-                "Reason: " . 'Function Name does not exist - Check Method Name' . PHP_EOL .
-                "-------------------------" . PHP_EOL;
-            file_put_contents(__DIR__ . '/logs/background_jobs_errors.log.log', $log, FILE_APPEND);
-
+            $this->genericMessage('ERROR CHECK METHOD EXISTS - ');
             return false;
         }
 
+    }
+
+    /**
+     * @param $message
+     * @return void
+     */
+    public function genericMessage($message) {
+        $log = "{$message}: CLASS:" . $this->class . ' -  METHOD: ' . $this->method . ' - ' . date("F j, Y, g:i a") . PHP_EOL .
+            "Attempt: " . ($this->class . ' | ' . $this->method) . PHP_EOL .
+            "User: " . 'Authenticated User??' . PHP_EOL .
+            "-------------------------" . PHP_EOL;
+        file_put_contents(__DIR__ . '/logs/background_job_info_log.log', $log, FILE_APPEND);
     }
 
 
